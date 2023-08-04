2412160fd55633dea71fe4a71808f73a4c98a87f
WarrenU <umbach4@gmail.com>
2023-08-04 13:51:39 -0500

    moved log functions and added time pause

--- FirstTest.py
+++ FirstTest.py
@@ -6,7 +6,7 @@ import csv
 from datetime import datetime
 from datetime import date
 from callBackFunctions import *
-from logFunctions import *
+import time
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
@@ -22,12 +22,25 @@ timing_advance = 0
 timestamp = datetime.now()
 logFilename = "data_logging_" + str(date.today()) + ".csv"
 
+def setupLogFile(fileName):
+    
+    header = ['TIME','SPEED','RPM', 'Intake Temp.', 'MAF (g/s)', 'Engine Load', 'Fuel Rail Press.', 'AFR', 'Long B2 Trim', 'Timing Adv.']
+    with open(fileName, "w", newline="") as dl:
+        writer = csv.writer(dl)
+        writer.writerow(header)
+
+
+def log_to_file(fileName):
+    timestamp = datetime.now()
+    row = [str(timestamp),str(speed), str(rpm), str(intake_temp), str(maf), str(load), str(fuel_rail_press), str(afr), str(o2_trim), str(timing_advance)]
+    with open(fileName, "a", newline="") as dl:
+        writer = csv.writer(dl)
+        writer.writerow(row)
+
 carConnected = False
 connection = obd.Async(fast=False)
-print('Waiting for Connection...')
-while carConnected == False:
+while len(connection.supported_commands) < 100:
     connection = obd.Async(fast=False)
-    carConnected = obd.Async.is_connected()
 
 print('Connection Established')
 
@@ -56,6 +69,7 @@ ecu_connections()
 run = True
 logging = True
 while run:
+    time.sleep(0.25)
     if logging:
         print(rpm)
         log_to_file(logFilename)
