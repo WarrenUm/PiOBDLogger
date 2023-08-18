b94b158e915811245e6462a1efef50ab68214ab6
WarrenU <umbach4@gmail.com>
2023-08-17 22:40:52 -0500

    added more pid functions to handle callbacks

--- FirstTest.py
+++ FirstTest.py
@@ -12,6 +12,28 @@ roundDigits = 1
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
+def getMeasure(measure):
+    return round(float(measure),roundDigits)
+
+def trimPercent(percentInput):
+    return getMeasure(str(percentInput).replace("percent",""))
+
+def trimDegree(degreeInput):
+    return getMeasure(str(degreeInput).replace("degree",""))
+
+def toFahrenheit(celciusInput):
+    return getMeasure((float(celciusInput) * 1.8) + 32)
+
+def trimUnit(measure,unitString):
+    return getMeasure(str(measure).replace(unitString,""))
+
+def kpToPsi(inputKp):
+    return getMeasure(float(inputKp) * 0.145038)
+
+def kphToMph(inputKph):
+    return getMeasure(float(inputKph))
+
+
 def get_speed(s):
     global speed
     if not s.is_null():
@@ -66,27 +88,116 @@ def get_o2(o):
     if not o.is_null():
         o2_trim = trimPercent(o.value)  # +/- 3 percent normal range - negative = rich, positive = lean
 
-def getMeasure(measure):
-    return round(float(measure),roundDigits)
+def getShortFuelTrim_1(shortFuelTrim_1Callback):
+    global shortFuelTrim_1
+    if not shortFuelTrim_1Callback.is_null():
+        shortFuelTrim_1 = trimPercent(shortFuelTrim_1Callback.value)
 
-def trimPercent(percentInput):
-    return getMeasure(str(percentInput).replace("percent",""))
+def getCalibrationId(calibrationIdCallback):
+    #test return value
+    global calibrationId
+    if not calibrationIdCallback.is_null():
+        calibrationId = str(calibrationIdCallback.value)
 
-def trimDegree(degreeInput):
-    return getMeasure(str(degreeInput).replace("degree",""))
+def getCatalystTemp_B1S1(catalystTemp_B1S1Callback):
+    global catalystTemp_B1S1
+    if not catalystTemp_B1S1Callback.is_null():
+        catalystTemp_B1S1 = toFahrenheit(catalystTemp_B1S1Callback.value.magnitude)
 
-def toFahrenheit(celciusInput):
-    return getMeasure((float(celciusInput) * 1.8) + 32)
+def getControlModuleVoltage(controlModuleVoltageCallback):
+    global controlModuleVoltage
+    if not controlModuleVoltageCallback.is_null():
+        controlModuleVoltage = getMeasure(controlModuleVoltageCallback.value.magnitude)
 
-def trimUnit(measure,unitString):
-    return getMeasure(str(measure).replace(unitString,""))
+def getObdCompliance(obdComplianceCallback):
+    global obdCompliance
+    if not obdComplianceCallback.is_null():
+        obdCompliance = str(obdComplianceCallback.value)
 
-def kpToPsi(inputKp):
-    return getMeasure(float(inputKp) * 0.145038)
+def getThrottlePosition(throttlePositionCallback):
+    global throttlePosition
+    if not throttlePositionCallback.is_null():
+        throttlePosition = trimPercent(throttlePositionCallback.value)
 
-def kphToMph(inputKph):
-    return getMeasure(float(inputKph))
+def getO2_B1S1(o2_B1S1Callback):
+    global o2_B1S1
+    if not o2_B1S1Callback.is_null():
+        o2_B1S1 = getMeasure(o2_B1S1Callback.value.magnitude)
 
+def getAcceleratorPositionE(acceleratorPositionECallback):
+    global acceleratorPositionE
+    if not acceleratorPositionECallback.is_null():
+        acceleratorPositionE = trimPercent(acceleratorPositionECallback.value)
+
+def getEngineRuntime(engineRuntimeCallback):
+    global engineRuntime
+    if not engineRuntimeCallback.is_null():
+        engineRuntime = getMeasure(engineRuntimeCallback.value.magnitude)
+
+def getCommandedEquivalenceRatio(commandedEquivalenceRatioCallback):
+    global commandedEquivalenceRatio
+    if not commandedEquivalenceRatioCallback.is_null():
+        commandedEquivalenceRatio = getMeasure(commandedEquivalenceRatioCallback.value.magnitude)
+
+def getO2_B1S2(o2_B1S2Callback):
+    global o2_B1S2
+    if not o2_B1S2Callback.is_null():
+        o2_B1S2 = getMeasure(o2_B1S2Callback.value.magnitude)
+
+def getRelativeThrottlePosition(relativeThrottlePositionCallback):
+    global relativeThrottlePosition
+    if not relativeThrottlePositionCallback.is_null():
+        relativeThrottlePosition = trimPercent(relativeThrottlePositionCallback.value)
+
+def getElmVoltage(elmVoltageCallback):
+    global elmVoltage
+    if not elmVoltageCallback.is_null():
+        elmVoltage = getMeasure(elmVoltageCallback.value.magnitude)
+
+def getAcceleratorPositionD(acceleratorPositionDCallback):
+    global acceleratorPositionD
+    if not acceleratorPositionDCallback.is_null():
+        acceleratorPositionD = trimPercent(acceleratorPositionDCallback.value)
+
+def getCommandedEvaporativePurge(commandedEvaporativePurgeCallback):
+    global commandedEvaporativePurge
+    if not commandedEvaporativePurgeCallback.is_null():
+        commandedEvaporativePurge = trimPercent(commandedEvaporativePurgeCallback.value)
+
+def getFuelPressure(fuelPressureCallback):
+    global fuelPressure
+    if not fuelPressureCallback.is_null():
+        fuelPressure = kpToPsi(fuelPressureCallback.value.magnitude)
+
+def getLongFuelTrim_1(longFuelTrim_1Callback):
+    global longFuelTrim_1
+    if not longFuelTrim_1Callback.is_null():
+        longFuelTrim_1 = trimPercent(longFuelTrim_1Callback.value)
+
+def getCommandedThrottleActuator(commandedThrottleActuatorCallback):
+    global commandedThrottleActuator
+    if not commandedThrottleActuatorCallback.is_null():
+        commandedThrottleActuator = trimPercent(commandedThrottleActuatorCallback.value)
+
+def getFuelLevel(fuelLevelCallback):
+    global fuelLevel
+    if not fuelLevelCallback.is_null():
+        fuelLevel = trimPercent(fuelLevelCallback.value)
+
+def getThrottlePosition_B(throttlePosition_BCallback):
+    global throttlePosition_B
+    if not throttlePosition_BCallback.is_null():
+        throttlePosition_B = trimPercent(throttlePosition_BCallback.value)
+
+def getAbsoluteLoad(absoluteLoadCallback):
+    global absoluteLoad
+    if not absoluteLoadCallback.is_null():
+        absoluteLoad = trimPercent(absoluteLoadCallback.value)
+
+def getAmbientAirTemperature(ambientAirTemperatureCallback):
+    global ambientAirTemperature
+    if not ambientAirTemperatureCallback.is_null():
+        ambientAirTemperature = toFahrenheit(ambientAirTemperatureCallback.value.magnitude)
 
 speed = 0
 rpm = 0
@@ -138,7 +249,7 @@ def ecu_connections():
     connection.watch(obd.commands.COMMANDED_EQUIV_RATIO, callback=get_afr)
     connection.watch(obd.commands.MAF, callback=get_maf)
     connection.watch(obd.commands.TIMING_ADVANCE, callback=get_timing_a)
-    connection.watch(obd.commands.LONG_O2_TRIM_B1, callback=get_o2)
+    connection.watch(obd.commands.SHORT_FUEL_TRIM_1, callback=getShortFuelTrim_1)
 
     connection.start()
 
